01c0ce726ec240930fa7da9dc497f522e6acee61
Akshay <aksrahangdale@gmail.com>
2023-12-27 03:13:08 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Login, Signup } from "./pages";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
